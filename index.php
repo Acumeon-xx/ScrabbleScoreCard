8ae460d22f55f165f5f1e3337cf91c3532dce98d
Eric Howell <ehh1974@gmail.com>
2012-02-22 13:22:40 -0800

    Local commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Scrabble Score Card</h2>
+        <form action="newgame.php" method="get">
+            <input type="submit" value="New Game">
+        </form> 
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
