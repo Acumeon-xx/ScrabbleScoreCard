05cb535f14c31edcf633e3308e6914097e40afc0
Eric Howell <ehh1974@gmail.com>
2012-02-25 01:51:50 -0800

    Local commit

--- newgame.php
+++ newgame.php
@@ -1,5 +1,16 @@
-<?php
-
-echo "New Game";
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Scrabble Score Card:New Game</title>
+    </head>
+    <body>
+        <h2>New Game</h2>
+        <form action="newgame.php" method="get">
+            <input type="submit" value="New Game">
+        </form> 
+        <?php
+            
+        ?>
+    </body>
+</html>
