2c6f856d5e2cc5f3d66f136a09cbd87781773556
Eric Howell <ehh1974@gmail.com>
2012-02-26 15:27:02 -0800

    Local commit

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php require_once"library/header.php"; ?>
         <a href="newgame.php">New Game</a>
+        <a href="advsearch.php">Advanced Search</a>
         <form name="search" action="search.php" method="get">
-            <text name="searchString" value="" />
+            <text name="searchString" value="Player Name" />
             <input name="submit" value="Search" />
         </form>
         <a href="search.php">Search</a>
