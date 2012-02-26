6458341f9926f22e0a594dff7c9c0bbec338c7e5
Eric Howell <ehh1974@gmail.com>
2012-02-26 15:22:43 -0800

    Local commit

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h2>Scrabble Score Card</h2>
-        <form action="newgame.php" method="get">
-            <input type="submit" value="New Game">
-        </form> 
+<?php require_once"library/header.php"; ?>
+        <a href="newgame.php">New Game</a>
+        <form name="search" action="search.php" method="get">
+            <text name="searchString" value="" />
+            <input name="submit" value="Search" />
+        </form>
+        <a href="search.php">Search</a>
         <?php
-            
+            require_once"library/footer.php"; 
         ?>
-    </body>
-</html>
--- library/header.php
+++ library/header.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Scrabble Score Card</title>
+        <script type="text/javascript" src="library/js/site.js"></script>
     </head>
     <body>
         <h2>Scrabble Score Card</h2>
