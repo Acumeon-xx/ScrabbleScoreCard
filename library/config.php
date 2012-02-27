e22d3810ceca182716efb460982c8a0fb1faeaf5
Eric Howell <ehh1974@gmail.com>
2012-02-26 19:18:59 -0800

    Update library/config.php

--- library/config.php
+++ library/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // Globals
-define("DB_SERVER", "db405294162.db.1and1.com");
-define("DB_USER", "dbo405294162");
-define("DB_PASS", "01D01C20$db11");
-define("DB_NAME", "db405294162");
+define("DB_SERVER", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 ?>
